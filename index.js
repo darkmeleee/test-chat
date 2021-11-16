61376d8f457366bbcdd12d3235e1999a7793f861
Michael <93446680+darkmeleee@users.noreply.github.com>
2021-11-16 21:21:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const  cookieParser = require('cookie-parser');
 var connectionsCount = 0;
+const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser('secret key'));
 
@@ -43,6 +44,6 @@ app.get('/get-cookie', (req, res) => {
 
 
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(PORT, () => {
+  console.log('listening on ' + PORT);
 });
